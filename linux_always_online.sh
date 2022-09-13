207d978972189a97f38d5ad2a8b48cd1ae4a9fac
Ishikawa <96659654+b71db892@users.noreply.github.com>
2022-09-13 19:43:39 +0800

    init push

--- linux_always_online.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /usr/local/share/SrunLoginScript
-source /usr/local/miniconda3/bin/activate base
-python always_online.py 2>&1
--- win_always_online.vbs
+++
@@ -1,3 +0,0 @@
-set ws=createobject("wscript.shell")
-REM 新建一个脚本对象
-ws.run "C:\programs\SrunLoginScript\win_always_online.bat",0
